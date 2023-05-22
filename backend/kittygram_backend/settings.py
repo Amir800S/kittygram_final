ada43786e9f42caec9be022bb93aab93fdbf9963
Your Name <you@example.com>
2023-05-22 10:30:50 +0600

    work

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -74,10 +74,10 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-{'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-{'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-{'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-{'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+	{'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+	{'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+	{'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+	{'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
 LANGUAGE_CODE = 'en-us'
@@ -108,8 +108,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 
-    'DEFAULT_PAGINATION_CLASS':
-	 ['rest_framework.pagination.PageNumberPagination'],
+    'DEFAULT_PAGINATION_CLASS': [
+	'rest_framework.pagination.PageNumberPagination'
+    ],
     'PAGE_SIZE': 10,
 
 }
