33ffedab01dbc10d6926ee4d5d5527d44358e4b8
Your Name <you@example.com>
2023-05-30 18:33:54 +0600

    on review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1', 'localhost', '158.160.16.192', 'kittyhostdev.ddns.net',
